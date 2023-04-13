8d972ef1aac4b6debce1149cefec221e7037bbee
Canoa furada =) <88066241+canoafurada2021@users.noreply.github.com>
2023-04-12 22:22:18 -0300

    Delete teste directory

--- teste/lib/constants.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-const primaryColor = Color(0xFF2697FF);
-const secondaryColor = Color(0xFF5D668D);
-const bgColor = Color(0xFF212332);
-
-const defaultPadding = 16.0;
